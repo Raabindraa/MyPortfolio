526700774be851d4783e4ae5bc82c5daff6c9558
Rabindra khadka <42757077+Raabindraa@users.noreply.github.com>
2025-06-02 17:43:32 +0545

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -150,6 +150,24 @@ const educationInfo = {
 };
 
 // Your top 3 proficient stacks/tech experience
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
 
 // Work experience section
 
@@ -375,6 +393,7 @@ export {
   splashScreen,
   skillsSection,
   educationInfo,
+  techStack,
   workExperiences,
   openSource,
   bigProjects,
